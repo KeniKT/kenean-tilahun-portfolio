9c448762a51afd344cfd31f4d4f351f5558f1739
Kenean Tilahun <keni232127@gmail.com>
2025-08-06 09:05:30 +0300

    pages add

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import Skills from './pages/Skills';
+import Experience from './pages/Experience';
 import './App.css';
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
@@ -24,3 +28,4 @@ function App() {
 }
 
 export default App;
+
